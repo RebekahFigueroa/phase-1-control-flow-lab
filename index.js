cdcca9b60fd19203acc9ddda288a336f8e5a2452
Rebekah <figueroarebekah@gmail.com>
2022-12-12 15:46:23 -0600

    commit tree 1e5341ac52174b2af34c9ee4e89b8093ee9e9be2

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(i) {
   // Write your code here!
+  if (i <= 400) {
+    return "This one is on me!";
+  } else if (i > 400 && i <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (i > 2000 && i <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
